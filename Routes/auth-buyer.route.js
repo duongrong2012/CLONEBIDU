b4234ce1e2da2496b556e5456016a7453bf0b621
Minh Tri <duongrong2012@gmail.com>
2025-05-14 09:42:18 +0700

    fix (base src) : fix src base

--- Routes/auth-buyer.route.js
+++ Routes/auth-buyer.route.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../Controllers/auth.controller');
 
 // Public routes
-router.post('/register', authController.register);
 
 module.exports = router;
